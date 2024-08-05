294636d31c75311027737c6ef69640214c45384f
gustavof04 <fernandes.gustavo2910@gmail.com>
2024-08-04 20:38:00 -0400

    feat: MUI icons, create contact page

--- frontend/src/pages/ContactPage/index.tsx
+++ frontend/src/pages/ContactPage/index.tsx
@@ -1,13 +1,38 @@
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import styles from "./ContactPage.module.css";
 
 const ContactPage = () => {
   return (
     <>
       <Header />
       <Container>
-        <h2>Contato</h2>
+        <section className={styles.contact}>
+          <h2>Contatos</h2>
+          <h3>Entre em contato</h3>
+          <p>Para que possamos conversar mais sobre a iniciativa.</p>
+          <div className={styles.icons}>
+            <a
+              href="mailto:fernandes.gustavo2910@gmail.com?subject=Dúvida sobre o Manaos Odyssey"
+              target="_blank"
+            >
+              <MailOutlineIcon fontSize="large" className={styles.icon} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/gustavofernandes04/"
+              target="_blank"
+            >
+              <LinkedInIcon fontSize="large" className={styles.icon} />
+            </a>
+            <a href="https://github.com/gustavof04" target="_blank">
+              <GitHubIcon fontSize="large" className={styles.icon} />
+            </a>
+          </div>
+        </section>
       </Container>
       <Footer />
     </>
