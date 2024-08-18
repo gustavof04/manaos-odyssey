f5fd95f51752fe609fe8d222560728261eb07d17
gustavof04 <fernandes.gustavo2910@gmail.com>
2024-08-18 16:43:30 -0400

    feat: Load attractions cards data from api

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,11 +1,16 @@
 import styles from "./Card.module.css";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
-const Card = () => {
+interface Props {
+  title: string;
+  description: string;
+}
+
+const Card = ({ title, description }: Props) => {
   return (
     <section className={styles.card}>
-      <h3>Título da Atração</h3>
-      <p>Texto descritivo da Atração.</p>
+      <h3>{title}</h3>
+      <p>{description}</p>
       <div className={styles.cardFooter}>
         <button className={styles.cardButton}>
           <ArrowRightAltIcon />
