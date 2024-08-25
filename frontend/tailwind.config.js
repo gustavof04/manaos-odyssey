c6298f1d1e2f9af0d9e47c16679559340435fd70
gustavof04 <fernandes.gustavo2910@gmail.com>
2024-08-25 18:58:20 -0400

    feat: Add tailwind, refact header component

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "primary": "#466156",
+      "secondary": "#6A8973",
+      "terciary": "#A3B993",
+      "soft-white": "#F1F1F1",
+      "white": "#FEFEFE",
+      "black": "#111",
+      "gray": "#222222",
+      "red": "#CF1415",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
