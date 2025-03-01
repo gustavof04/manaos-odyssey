34e069b81922ad0d324a6585ca80a0c901c511e7
Gustavo Fernandes <fernandes.gustavo2910@gmail.com>
2025-03-01 19:17:52 -0400

    style: Change footer year

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="w-full font-bold text-lg flex items-center justify-center p-2 bg-primary text-white md:h-12">
-      Feito com ❤️ por Gustavo Fernandes &copy; 2024.
+      Feito com ❤️ por Gustavo Fernandes &copy; 2025.
     </footer>
   );
 };
