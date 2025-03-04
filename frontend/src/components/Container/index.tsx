61dae6835dc2340883de76af948cc5dfa864ddab
Gustavo Fernandes <fernandes.gustavo2910@gmail.com>
2025-03-04 14:29:58 -0400

    style: Change container padding for better title position

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -3,7 +3,7 @@ type ContainerProps = {
 };
 
 const Container = ({ children }: ContainerProps) => {
-  return <section className="w-full min-h-[90vh] flex flex-col items-center gap-4 py-[1rem]">{children}</section>;
+  return <section className="w-full min-h-[90vh] flex flex-col items-center gap-4 py-[2.5rem]">{children}</section>;
 };
 
 export default Container;
